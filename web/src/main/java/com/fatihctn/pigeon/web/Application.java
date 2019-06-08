5572abaa1c52844b0d23a5e7d7420181880a4280
Fatih Çetin <f4tih.c@gmail.com>
2019-06-08 12:56:09 +0300

    EnableJPA and Entity fixed property

--- web/src/main/java/com/fatihctn/pigeon/web/Application.java
+++ web/src/main/java/com/fatihctn/pigeon/web/Application.java
@@ -1,11 +1,23 @@
 package com.fatihctn.pigeon.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = {"com.fatihctn.pigeon"})
+@EntityScan(basePackages = {"com.fatihctn.pigeon"})
+@EnableAsync
+@EnableScheduling
+@EnableConfigurationProperties
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
